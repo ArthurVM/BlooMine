19de38ee7a8a6b1fd939f4ca1d02957c3b5f5510
ArthurVM <ArthurVM@github.com>
2024-10-04 16:45:58 +0100

    --no-install flag in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+echo
+echo "Building..."
+echo
+
 mkdir build
 cd build
 cmake ..
 make
-make install
+
+# Only run make install if the --no-install argument wasn't provided
+if [[ "$1" != "--no-install" ]]; then
+  echo
+  echo "Installing..."
+  echo
+  make install
+fi
